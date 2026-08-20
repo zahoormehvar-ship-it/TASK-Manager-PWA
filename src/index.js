52b1c3e938a7e294b9e617b393e7594195eb3273
Mehvar <zahoormehvar@gmail.com>
2026-08-21 00:33:06 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .catch((err) => console.log('SW registration failed: ', err));
+  });
+}
+
